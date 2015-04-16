b3be2e53f9fe1b0a4ba88506e1df073719faf68d
David Gasquez <davidgasquez@gmail.com>
2015-04-16 18:48:37 +0200

    Add installation script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo
+echo "Updating system dotfiles:"
+
+# Get current directory
+current_directory=$(dirname $(readlink -f "$0"))
+
+# Bash
+echo " - bash"
+ln -sf $current_directory/bash/bashrc ~/.bashrc
+ln -sf $current_directory/bash/bash_aliases ~/.bash_aliases
+
+# Git
+echo " - gitconfig"
+ln -sf $current_directory/git/gitconfig ~/.gitconfig
+
+# xfce-terminal
+echo " - xfce4-terminal config"
+ln -sfT $current_directory/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
+
+# gtkrc-2.0
+echo " - gtkrc-2.0"
+ln -sf $current_directory/xfce4/gtkrc-2.0 ~/.gtkrc-2.0
+
+echo
+echo "Done!"
+echo
