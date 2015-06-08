c6be1ac263216a2c03a4882d91b9a671b25a9d25
David Gasquez <davidgasquez@gmail.com>
2015-06-08 10:34:32 +0200

    Add script to convert audio files into 320Kbps MP3s

--- install.sh
+++ install.sh
@@ -24,11 +24,6 @@ ln -sfT $current_directory/xfce4/xfce4-screenshooter ~/.config/xfce4/xfce4-scree
 echo " - gtkrc-2.0"
 ln -sf $current_directory/xfce4/gtkrc-2.0 ~/.gtkrc-2.0
 
-# Scripts
-echo " - scripts"
-mkdir -p ~/.scripts
-ln -sf $current_directory/scripts/select_sound_card ~/.scripts/select_sound_card
-
 # Task
 echo " - taskwarrior"
 ln -sf $current_directory/taskwarrior/taskrc ~/.taskrc
