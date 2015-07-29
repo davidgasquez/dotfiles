060b224cb763a1210e6e013ce2ad78174a0878fb
David Gasquez <davidgasquez@gmail.com>
2015-07-29 17:38:21 +0200

    Update installation script

--- install.sh
+++ install.sh
@@ -3,30 +3,23 @@
 echo
 echo "Updating system dotfiles:"
 
-# Get current directory
-current_directory=$(dirname $(readlink -f "$0"))
-
 # Bash
 echo " - bash"
-ln -sf $current_directory/bash/bashrc ~/.bashrc
-ln -sf $current_directory/bash/bash_aliases ~/.bash_aliases
+ln -sf ./bash/bashrc ~/.bashrc
+ln -sf ./bash/bash_aliases ~/.bash_aliases
 
 # Git
 echo " - gitconfig"
-ln -sf $current_directory/git/gitconfig ~/.gitconfig
+ln -sf ./git/gitconfig ~/.gitconfig
 
-# xfce-terminal
-echo " - xfce4-terminal config"
-ln -sfT $current_directory/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
-ln -sfT $current_directory/xfce4/xfce4-screenshooter ~/.config/xfce4/xfce4-screenshooter
-
-# gtkrc-2.0
-echo " - gtkrc-2.0"
-ln -sf $current_directory/xfce4/gtkrc-2.0 ~/.gtkrc-2.0
+# Terminator
+echo " - terminator"
+ln -sfT ./terminator/config ~/.config/terminator/config
+ln -sfT ./xfce4/xfce4-screenshooter ~/.config/xfce4/xfce4-screenshooter
 
 # Task
 echo " - taskwarrior"
-ln -sf $current_directory/taskwarrior/taskrc ~/.taskrc
+ln -sf ./taskwarrior/taskrc ~/.taskrc
 
 echo
 echo "Done!"
