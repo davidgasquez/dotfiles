788eb8a9db0d6043f6976bc96c24b5a0f6b36ad8
David Gasquez <davidgasquez@gmail.com>
2017-11-10 21:23:18 +0100

    Move polybar out

---
+++ polybar/music.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+icon=""
+
+player_status=$(playerctl status 2> /dev/null)
+
+if [[ $? -eq 0 ]]; then
+    metadata="$(playerctl metadata artist) - $(playerctl metadata title)"
+fi
+
+# Foreground color formatting tags are optional
+if [[ $player_status = "Playing" ]]; then
+    echo "$icon %{F#C8D6E9}$metadata"
+elif [[ $player_status = "Paused" ]]; then
+    echo "$icon %{F#888}$metadata"
+else
+    echo ""
+fi
