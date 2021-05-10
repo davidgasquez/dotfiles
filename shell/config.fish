cb1a5d4fa159b1a44d5281cdcd583402227effb8
David Gasquez <davidgasquez@gmail.com>
2021-05-10 16:25:51 +0200

    Use code insiders

--- shell/config.fish
+++ shell/config.fish
@@ -2,7 +2,8 @@
 set PATH /home/david/.local/bin/ $PATH
 
 # Editor
-alias c="code ."
+alias c="code-insiders --enable-features=UseOzonePlatform --ozone-platform=wayland"
+alias code="code-insiders --enable-features=UseOzonePlatform --ozone-platform=wayland"
 
 # System Management
 alias up="yay -Syu --noconfirm --noansweredit --sudoloop"
