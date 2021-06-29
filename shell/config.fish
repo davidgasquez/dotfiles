22a9eeec7bf6044947acb1a03f419bd110c8fa01
David Gasquez <davidgasquez@gmail.com>
2021-06-29 11:11:03 +0200

    :memo:

--- shell/config.fish
+++ shell/config.fish
@@ -2,28 +2,18 @@
 set PATH /home/david/.local/bin/ $PATH
 
 # Editor
-alias c="code-insiders --enable-features=UseOzonePlatform --ozone-platform=wayland ."
-alias code="code-insiders --enable-features=UseOzonePlatform --ozone-platform=wayland"
+alias c="code --enable-features=UseOzonePlatform --ozone-platform=wayland ."
+alias code="code --enable-features=UseOzonePlatform --ozone-platform=wayland"
 
 # System Management
 alias up="yay -Syu --noconfirm --noansweredit --sudoloop"
 alias upup="yay -Syu --noconfirm --noansweredit --devel --sudoloop"
-alias pastebin="curl -F c @- https://ptpb.pw"
 
 # Tasks
 alias t="todo --color l -f 'today'"
 alias tn="todo q"
 alias ts="todo s"
 alias td="todo c"
-
-# Docker
-alias drmsc="docker rm (docker ps -a -q)"
-alias drmdi="docker rmi (docker images -f dangling true -q)"
-alias dj="docker run -p 8888:8888 -it --rm davidgasquez/dopyter:latest"
-alias djlo="docker run -p 8888:8888 -it --user (id -u):(id -g) --rm -v $PWD:/work davidgasquez/dopyter:latest"
-
-# Custom
-alias handbook="code-insiders ~/projects/handbook"
 
 # SSH Agent
 function __ssh_agent_is_started -d "check if ssh agent is already started"
