dca955a5062d83962c51f446f73f8f776f04427f
David Gasquez <davidgasquez@gmail.com>
2025-06-30 09:51:15 +0200

    ➕ Add inter-font and font-awesome to font packages

--- fonts/packages.sh
+++ fonts/packages.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 packages=(
+    inter-font
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
     noto-fonts-extra
-    ttf-mac-fonts
-    ttf-ms-fonts
+    otf-font-awesome
     ttf-croscore
     ttf-dejavu
     ttf-droid
     ttf-jetbrains-mono-nerd
     ttf-liberation
+    ttf-mac-fonts
+    ttf-ms-fonts
     ttf-roboto
     ttf-ubuntu-font-family
 )
