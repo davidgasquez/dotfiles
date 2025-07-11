35b3052bda90b9541d1b35f2f95c1f0d822d9885
David Gasquez <davidgasquez@gmail.com>
2025-07-11 23:25:36 +0200

    ✨ Add UWSM and enable hyprpolkitagent service
    - Add universal wayland session manager (uwsm)
    - Enable hyprpolkitagent service instead of just starting it

--- hypr/setup.sh
+++ hypr/setup.sh
@@ -24,6 +24,7 @@ packages=(
     xdg-desktop-portal-gtk
     xdg-desktop-portal-hyprland
     xdg-utils
+    uwsm
 )
 
 # Install Hyprland and related packages
@@ -31,5 +32,5 @@ paru -S --needed --noconfirm "${packages[@]}"
 
 # Start Polkit agent for Hyprland (https://wiki.hypr.land/Hypr-Ecosystem/hyprpolkitagent/)
 if ! systemctl --user is-active --quiet hyprpolkitagent.service; then
-    systemctl --user start hyprpolkitagent.service
+    systemctl --user enable --now hyprpolkitagent.service
 fi
