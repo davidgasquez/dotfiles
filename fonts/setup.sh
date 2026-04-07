1730d66c63c31964bef18eab68f0088f27c464cd
David Gasquez <davidgasquez@gmail.com>
2026-04-07 09:41:33 +0200

    🎨 Standardize JetBrainsMono Nerd Font setup

--- fonts/setup.sh
+++ fonts/setup.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+DOTFILES=$(dirname "$(dirname "$(realpath "$0")")")
+FONTCONFIG_DIR="${HOME}/.config/fontconfig"
+
 packages=(
+    fontconfig
     inter-font
     noto-fonts
     noto-fonts-cjk
@@ -20,3 +24,8 @@ packages=(
 )
 
 paru -S --needed --noconfirm "${packages[@]}"
+
+mkdir -p "${FONTCONFIG_DIR}"
+ln -snf "${DOTFILES}/fonts/fontconfig/fonts.conf" "${FONTCONFIG_DIR}/fonts.conf"
+
+fc-cache -f
