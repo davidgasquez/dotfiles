e0c6f91de0706b8f07d1aa82f464ab746a52717f
David Gasquez <davidgasquez@gmail.com>
2026-07-24 18:57:52 +0200

    ♻️ Use standard font families

--- fonts/setup.sh
+++ fonts/setup.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 DOTFILES=$(dirname "$(dirname "$(realpath "$0")")")
-FONTCONFIG_DIR="${HOME}/.config/fontconfig"
+FONTCONFIG_DIR="${XDG_CONFIG_HOME:-${HOME}/.config}/fontconfig"
 
 packages=(
     fontconfig
@@ -10,20 +10,12 @@ packages=(
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
-    noto-fonts-extra
-    otf-font-awesome
-    ttf-croscore
-    ttf-dejavu
-    ttf-droid
-    ttf-jetbrains-mono-nerd
+    ttf-jetbrains-mono
     ttf-liberation
-    ttf-roboto
-    ttf-ubuntu-font-family
+    ttf-nerd-fonts-symbols
 )
 
 paru -S --needed --noconfirm "${packages[@]}"
 
 mkdir -p "${FONTCONFIG_DIR}"
 ln -snf "${DOTFILES}/fonts/fontconfig/fonts.conf" "${FONTCONFIG_DIR}/fonts.conf"
-
-fc-cache -f
